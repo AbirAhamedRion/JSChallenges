01845744a0b6914462305a963ae438f2f9f5d426
Abir Ahamed <abirahamed281102@gmail.com>
2024-05-27 00:16:21 +0600

    challenge ---5 complete & 6 started

--- app.js
+++ app.js
@@ -48,17 +48,34 @@
 
 //
 // Challenge ---4
-function tipCalc(bill) {
-    const tip = bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
-    return `The bill was ${bill}, the tip was ${+parseFloat(tip).toFixed(2)}, and the total value ${bill + +parseFloat(tip).toFixed(2)}`;
-}
+// function tipCalc(bill) {
+//     const tip = bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+//     return `The bill was ${bill}, the tip was ${+parseFloat(tip).toFixed(2)}, and the total value ${bill + +parseFloat(tip).toFixed(2)}`;
+// }
+//
+// console.log(tipCalc(275));
+// console.log(tipCalc(40));
+// console.log(tipCalc(430));
 
-console.log(tipCalc(275));
-console.log(tipCalc(40));
-console.log(tipCalc(430));
+// Challenge --5
 
-
-
-
-
+// const calcTeamAvg = (scoreArr) => {
+//     return scoreArr.reduce((acc, a) => acc + a, 0) / scoreArr.length;
+// };
+// const checkWinner = function (avgDolphins, avgKoalas) {
+//     if (avgDolphins >= avgKoalas * 2) {
+//         console.log(`Dolphins win (${avgDolphins}vs${avgKoalas}) `);
+//     } else if (avgKoalas >= avgDolphins * 2) {
+//         console.log(`Koalas  win (${avgKoalas}vs${avgDolphins})`);
+//     } else {
+//         console.log(`No result`);
+//     }
+// };
+// const avgDolphins1 = calcTeamAvg([44, 23, 71]);
+// const avgDolphins2 = calcTeamAvg([85, 54, 41]);
+// const avgKoalas1 = calcTeamAvg([65, 54, 49]);
+// const avgKoalas2 = calcTeamAvg([23, 34, 27]);
+//
+// checkWinner(avgDolphins1, avgKoalas1);
+// checkWinner(avgDolphins2, avgKoalas2);
 
