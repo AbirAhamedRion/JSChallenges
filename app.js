af6c68c53bceb56caa77b2ecace3c0d9e3046fb2
Abir Ahamed <abirahamed281102@gmail.com>
2024-05-26 23:52:17 +0600

    challenge --3 complete & 4 started

--- app.js
+++ app.js
@@ -1,36 +1,53 @@
-const markHeight = 1.69;
-const markMass = 78;
-const johnHight = 1.95;
-const johnMass = 92;
-
-function bmiGenarator(mass, height) {
-    return mass / height ** 2;
-}
-
-const markHigherBMI = bmiGenarator(markMass, markHeight) > bmiGenarator(johnMass, johnHight);
-const markHigherBMI2 = bmiGenarator(95, 1.88) > bmiGenarator(85, 1.76);
+// const markHeight = 1.69;
+// const markMass = 78;
+// const johnHight = 1.95;
+// const johnMass = 92;
+//
+// function bmiGenarator(mass, height) {
+//     return mass / height ** 2;
+// }
+//
+// const markHigherBMI = bmiGenarator(markMass, markHeight) > bmiGenarator(johnMass, johnHight);
+// const markHigherBMI2 = bmiGenarator(95, 1.88) > bmiGenarator(85, 1.76);
 // console.log(markHigherBMI);
 // console.log(markHigherBMI2);
 
 //Challenge --2
-if (markHigherBMI) {
-    console.log("Mark's BMI is higher than John's!");
-} else {
-    console.log("John's BMI is higher than Mark's!");
-}
-
-if (markHigherBMI) {
-    console.log(`Mark's
-    BMI (${parseFloat(bmiGenarator(markMass, markHeight)).toFixed(2)}) is higher than John's (${parseFloat(bmiGenarator(johnMass, johnHight)).toFixed(2)})`);
-} else {
-    console.log(`John's
-    BMI (${parseFloat(bmiGenarator(johnMass, johnHight)).toFixed(2)}) is higher than Mark's (${parseFloat(bmiGenarator(markMass, markHeight)).toFixed(2)})`);
-}
-
-
+// if (markHigherBMI) {
+//     console.log("Mark's BMI is higher than John's!");
+// } else {
+//     console.log("John's BMI is higher than Mark's!");
+// }
+//
+// if (markHigherBMI) {
+//     console.log(`Mark's
+//     BMI (${parseFloat(bmiGenarator(markMass, markHeight)).toFixed(2)}) is higher than John's (${parseFloat(bmiGenarator(johnMass, johnHight)).toFixed(2)})`);
+// } else {
+//     console.log(`John's
+//     BMI (${parseFloat(bmiGenarator(johnMass, johnHight)).toFixed(2)}) is higher than Mark's (${parseFloat(bmiGenarator(markMass, markHeight)).toFixed(2)})`);
+// }
 
 
+// Challenge --3
+// const avgScore = function (score) {
+//     return score.reduce((acc, a) => acc + a, 0) / score.length;
+// };
+//
+// const dolphins = [96, 108, 89];
+// const dolphinsAvg = parseFloat(avgScore(dolphins)).toFixed(2);
+// const koalas = [88, 91, 110];
+// const koalasAvg = parseFloat(avgScore(koalas)).toFixed(2);
+//
+// if (dolphinsAvg > koalasAvg && dolphinsAvg > 100) {
+//     console.log(`Dolphins Win`);
+// } else if (koalasAvg > dolphinsAvg && koalasAvg > 100) {
+//     console.log(`koalas Win`);
+// } else {
+//     console.log(`Match drawn`);
+// }
 
+//
+// Challenge ---4
 
 
 
