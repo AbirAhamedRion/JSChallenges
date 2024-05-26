2a31de79e30640d1758988f59e8dd84989babddd
Abir Ahamed <abirahamed281102@gmail.com>
2024-05-26 12:52:47 +0600

    challenge --2 complete

--- app.js
+++ app.js
@@ -9,5 +9,31 @@ function bmiGenarator(mass, height) {
 
 const markHigherBMI = bmiGenarator(markMass, markHeight) > bmiGenarator(johnMass, johnHight);
 const markHigherBMI2 = bmiGenarator(95, 1.88) > bmiGenarator(85, 1.76);
-console.log(markHigherBMI);
-console.log(markHigherBMI2);
+// console.log(markHigherBMI);
+// console.log(markHigherBMI2);
+
+//Challenge --2
+if (markHigherBMI) {
+    console.log("Mark's BMI is higher than John's!");
+} else {
+    console.log("John's BMI is higher than Mark's!");
+}
+
+if (markHigherBMI) {
+    console.log(`Mark's
+    BMI (${parseFloat(bmiGenarator(markMass, markHeight)).toFixed(2)}) is higher than John's (${parseFloat(bmiGenarator(johnMass, johnHight)).toFixed(2)})`);
+} else {
+    console.log(`John's
+    BMI (${parseFloat(bmiGenarator(johnMass, johnHight)).toFixed(2)}) is higher than Mark's (${parseFloat(bmiGenarator(markMass, markHeight)).toFixed(2)})`);
+}
+
+
+
+
+
+
+
+
+
+
+
